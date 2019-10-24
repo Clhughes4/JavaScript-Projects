b69a8432ac82ebe49ab01af84a8953e040bf75a1
Clint Hughes <hughes.excelerate@gmail.com>
2019-10-24 07:57:23 -0700

    add Basic_JavaScript_Projets folder

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,35 @@
+window.alert("Hello! & Welcome");
+
+
+var X = "This is a string";
+document.write(X);
+
+
+var Text = "How do you do this?";
+document.write(Text);
+
+
+document.write("Gabriel asked, \"can I have some ice cream with sprinkles?\"<br>\"Dad responded, \"as long as you eat your food.\"");
+
+
+var A = "\"Be who you are and say what you feel because"
++" those who mind \don't matter and those that matter"
++" \don't mind\"" 
++"-Doctor Suess"
+document.write(A);
+
+
+var B = "I'm a" + " student";
+document.write(B);
+
+
+var Family = "Hughes", Dad = "Clint", Mom = "Aly", Son = "Gabriel", Daughter = "Lilly", Pet = "Callie";
+var Dad = Dad.fontcolor("blue");
+var Mom = Mom.fontcolor("green");
+var Son = Son.fontcolor("red");
+var Daughter = Daughter.fontcolor("purple");
+var Pet = Pet.fontcolor("grey");
+document.write(Mom, Dad, Daughter, Son, Pet);
+
+
+document.write(75 + 45 - 37 + 47 + 16 - 47);
